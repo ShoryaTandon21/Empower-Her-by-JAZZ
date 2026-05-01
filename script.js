0e2af8bf7282a3f647b8e8ccd548ae3101f1c7c1
Shorya Tandon <shoryatandon21@gmail.com>
2026-05-02 03:42:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function(){
 
-  // scroll
+  window.toggleMenu = function(){
+    let nav = document.getElementById("navLinks");
+    nav.style.display = nav.style.display==="block" ? "none" : "block";
+  }
+
   window.scrollToSection = function(id){
     document.getElementById(id).scrollIntoView({behavior:"smooth"});
   }
 
-  // menu
-  window.toggleMenu = function(){
-    let menu = document.getElementById("navLinks");
-    menu.style.display = menu.style.display==="flex"?"none":"flex";
-  }
-
-  // whatsapp
   window.openWhatsApp = function(){
     window.open("https://wa.me/919711771383");
   }
 
-  // payment
   window.payNow = function(){
     window.open("https://rzp.io/l/YOUR_LINK");
   }
