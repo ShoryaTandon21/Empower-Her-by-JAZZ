312f184961ebb7404c74d617a51facc53f03460f
Shorya Tandon <shoryatandon21@gmail.com>
2026-05-01 19:03:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-let slides = document.querySelectorAll(".slide");
-let i = 0;
+document.addEventListener("DOMContentLoaded", function () {
 
-setInterval(() => {
-  slides[i].classList.remove("active");
-  i = (i + 1) % slides.length;
-  slides[i].classList.add("active");
-}, 3000);
+  let slides = document.querySelectorAll(".slide");
+  let i = 0;
 
-/* GOOGLE SHEET CONNECTION */
-const scriptURL = "https://script.google.com/macros/s/AKfycbwWUZWRLr7ncGtT-cVdQQy8_a04d1kluRQvyCQ6e_Nvzfk3wv8ThTcV3-Wf-Fmz0hl4cA/exec";
-const form = document.getElementById("form");
+  setInterval(() => {
+    slides[i].classList.remove("active");
+    i = (i + 1) % slides.length;
+    slides[i].classList.add("active");
+  }, 3000);
 
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  fetch(scriptURL, { method: "POST", body: new FormData(form)})
-  .then(() => alert("Submitted Successfully"))
-  .catch(() => alert("Error"));
+  const scriptURL = "https://script.google.com/macros/s/AKfycbwWUZWRLr7ncGtT-cVdQQy8_a04d1kluRQvyCQ6e_Nvzfk3wv8ThTcV3-Wf-Fmz0hl4cA/exec";
+  const form = document.getElementById("form");
+
+  if (form) {
+    form.addEventListener("submit", e => {
+      e.preventDefault();
+
+      fetch(scriptURL, {
+        method: "POST",
+        body: new FormData(form)
+      })
+      .then(() => {
+        alert("Submitted Successfully ✅");
+        form.reset();
+      })
+      .catch(() => {
+        alert("Error ❌");
+      });
+    });
+  }
+
 });
