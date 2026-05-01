8159783d3eee15011282e3f08567231e25af0b30
Shorya Tandon <shoryatandon21@gmail.com>
2026-05-02 04:21:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ function scrollToSection(id){
   document.getElementById(id).scrollIntoView({behavior:"smooth"});
 }
 
+/* 🔥 SLIDER FIX */
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function showSlide(){
+  slides.forEach(s => s.classList.remove("active"));
+  slides[index].classList.add("active");
+
+  index = (index + 1) % slides.length;
+}
+
+setInterval(showSlide, 3000);
+
+/* WhatsApp */
 function openWhatsApp(){
   window.open("https://wa.me/919711771383");
 }
