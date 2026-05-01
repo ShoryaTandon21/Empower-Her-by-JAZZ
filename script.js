bb6a462767c6c404b87e38cd8af708da4b0ed30c
Shorya Tandon <shoryatandon21@gmail.com>
2026-05-01 17:43:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ setInterval(() => {
   i = (i + 1) % slides.length;
   slides[i].classList.add("active");
 }, 3000);
+
+/* GOOGLE SHEET CONNECTION */
+const scriptURL = "https://script.google.com/macros/s/AKfycbwWUZWRLr7ncGtT-cVdQQy8_a04d1kluRQvyCQ6e_Nvzfk3wv8ThTcV3-Wf-Fmz0hl4cA/exec";
+const form = document.getElementById("form");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form)})
+  .then(() => alert("Submitted Successfully"))
+  .catch(() => alert("Error"));
+});
